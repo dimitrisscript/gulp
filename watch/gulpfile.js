9f971bbae38d334681cd2fbced6285a0fbbb086f
Dimitris Siakavelis <dimitris.siakavelis@dpdk.com>
2020-05-24 00:59:29 +0200

    added watch configuration

---
+++ watch/gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+const { parallel  } = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-terser');
+var cleanCSS = require('gulp-clean-css');
+var order = require("gulp-order");
+
+const image = require('gulp-image');
+const { watch } = require('gulp');
+
+
+
+function scripts() {
+	return gulp.src('js/*.js')
+		.pipe(order([
+			"jquery-3.3.1.min.js",
+			"jquery-migrate-3.0.0.min.js",
+			"mmenu.min.js",
+			"tippy.all.min.js",
+			"simplebar.min.js",
+			"bootstrap-slider.min.js",
+			"bootstrap-select.min.js",
+			"snackbar.js",
+			"clipboard.js",
+			"anime.min.js",
+			"counterup.min.js",
+			"magnific-popup.min.js",
+			"slick.min.js",
+			"custom.js",
+			"canvas-to-blob.min.js",
+			"moment.min.js",
+			"eucalls.js",
+		]))
+		.pipe(concat('home.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+};
+
+
+function css() {
+	return gulp.src('css/*.css')
+		.pipe(cleanCSS())
+		.pipe(concat('home.min.css'))
+		.pipe(gulp.dest('dist'))
+};
+
+function img() {
+	return gulp.src('img/*')
+		.pipe(image())
+		.pipe(gulp.dest('./dist'));
+}
+
+
+exports.default = function() {
+    watch(['css/*.css', 'js/*.js', 'src/img/*'], parallel(scripts, css, img));
+}
